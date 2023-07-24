68bf057b1fed371d93ad6e2330e8a504497c9be2
LucaPozzato <65533078+LucaPozzato@users.noreply.github.com>
2023-07-24 03:55:21 +0200

    Added bash script tester

--- Sviluppo/tester.py
+++ Sviluppo/tester.py
@@ -1,11 +1,8 @@
 import os
-from datetime import datetime
+# from datetime import datetime
 
-file = open("/Users/luca/Documents/Progetto-API/Sviluppo/results.txt", "w")
-
-for i in range(1, 112):
+for i in range(1, 50):
     print("test number: " + str(i))
-    start_time = datetime.now()
-    os.system("./progetto /Users/luca/Documents/Progetto-API/Test/archivio_test_aperti/open_" + str(i) + ".txt")
-    file.write("Test file: " + str(i) + str(' -> time: {}\n'.format(datetime.now() - start_time)))
-    os.system("diff output.txt /Users/luca/Documents/Progetto-API/Test/archivio_test_aperti/open_" + str(i) + ".output.txt")
+    os.system("echo 'test number: " + str(i) +"' >>results_test.txt && /usr/bin/time -h ./progetto /Users/luca/Documents/Progetto-API/Test/archivio_test_aperti/open_" + str(i) + ".txt >>results_test.txt 2>&1")
+    # file.write("Test file: " + str(i) + str(' -> time: {}\n'.format(datetime.now() - start_time)))
+    os.system("diff output.txt /Users/luca/Documents/Progetto-API/Test/archivio_test_aperti/open_" + str(i) + ".output.txt >>results_test.txt 2>&1 && echo '\n' >>results_test.txt")
---
+++ run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm results_test.txt
+gcc -Wall -Werror -std=gnu11 -O2 -lm Sviluppo/progetto.c -o Sviluppo/progetto
+
+for i in {1..111}
+do
+    echo test number: $i >>results_test.txt 
+    /usr/bin/time -h ./Sviluppo/progetto Test/archivio_test_aperti/open_$i.txt >>results_test.txt 2>&1
+    diff -u Sviluppo/output.txt /Users/luca/Documents/Progetto-API/Test/archivio_test_aperti/open_$i.output.txt >>results_test.txt 2>&1
+    echo ' ' >>results_test.txt
+    echo ' ' >>results_test.txt
+done
