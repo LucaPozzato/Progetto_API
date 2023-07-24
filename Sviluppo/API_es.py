c1649c58f825a31d71e06fb2169680ab186cec0f
LucaPozzato <65533078+LucaPozzato@users.noreply.github.com>
2023-07-24 16:07:37 +0200

    tester scrippettino bash

--- Sviluppo/API_es.py
+++ Sviluppo/API_es.py
@@ -1,23 +1,17 @@
-array = [1,7,19,21,3,4,5,10,12,8]
-i = -1
-j = -1
-for w in range(0, len(array)):
-    for k in range(0, len(array)):
-        if i == -1:
-            i = array.pop(0)
-        elif j == -1:
-            j = array.pop(0)
-        if j != -1 and i != -1:
-            if i > j:
-                array.append(j)
-                j = -1
-            else:
-                array.append(i)
-                i = -1
-    if i == -1:
-        array.append(j)
-        j = -1
-    elif j == -1:
-        array.append(i)
-        i = -1
-print(array)
+x = 3
+array = [[4,5],[2,x]]
+n = len(array[0])
+i = 0
+j = 0
+flag = 0
+while i < n and j < n:
+    if array[i][j] == x:
+        flag = 1
+    if array[i][j] < x:
+        j += 1
+    else:
+        i += 1
+if flag:
+    print("\ntrovato\n")
+else:
+    print("\nnon trovato\n")
