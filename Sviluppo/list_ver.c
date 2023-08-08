2fda598e835c241509c783136b1fa610db572cf7
LucaPozzato <65533078+LucaPozzato@users.noreply.github.com>
2023-08-09 03:03:31 +0400

    smaller range for bfs_sx and added max car -> 24

--- Sviluppo/list_ver.c
+++ Sviluppo/list_ver.c
@@ -5,6 +5,7 @@
 typedef struct stn
 {
     int id;
+    int m_car;
     int *cars;
     int len_cars;
     int color;
@@ -17,14 +18,14 @@ typedef struct q_str {
     struct stn *father;
 } q_str;
 
-stn* add_station (stn *first_stn, stn *new_stn, int station, int *cars, int len_cars);
+stn* add_station (stn *first_stn, stn *new_stn, int station, int *cars, int len_cars, int m_car);
 stn* del_station (stn *first_stn, int station, int *f);
 int add_car (stn *first_stn, int station, int car);
 int del_car (stn *first_stn, int *new_cars, int station, int car);
 int search_path (stn *first_stn, int stn_start, int stn_end, int *path, int n_stations);
-int bfs (stn *end_stn, q_str *queue, int direction);
+int bfs_dx (stn *end_stn, q_str *queue, int direction);
+int bfs_sx (stn *end_stn, q_str *queue, int direction, int m_car);
 int rebuild_graph (stn *start_stn, stn *end_stn, stn **next_nodes);
-int max_car (int *cars, int len_cars);
 
 int main (int argc, char *argv[])
 {
@@ -81,24 +82,28 @@ int main (int argc, char *argv[])
     while ((line_read = getline(&line, &line_len, file_input)) != -1) {
         command = strtok(line, " ");
         if (strcmp(command, "aggiungi-stazione") == 0) {
-                station = (int)strtol(strtok(NULL, " "), NULL, 10);
-                len_cars = (int)strtol(strtok(NULL, " "), NULL, 10);
-                cars = (int *) calloc(len_cars, sizeof(int));
-                for (int i = 0; i < len_cars; i++) {
-                    cars[i] = (int)strtol(strtok(NULL, " "), NULL, 10); 
+            int m_car = 0;
+            station = (int)strtol(strtok(NULL, " "), NULL, 10);
+            len_cars = (int)strtol(strtok(NULL, " "), NULL, 10);
+            cars = (int *) calloc(len_cars, sizeof(int));
+            for (int i = 0; i < len_cars; i++) {
+                cars[i] = (int)strtol(strtok(NULL, " "), NULL, 10);
+                if (cars[i] > m_car) {
+                    m_car = cars[i];
                 }
-                new_stn = (stn *) calloc(1, sizeof(stn));
-                result = add_station(first_stn, new_stn, station, cars, len_cars);
-                if (result != NULL) {
-                    first_stn = result;
-                    fprintf(file_output, "aggiunta\n");
-                    n_stations++;
-                }
-                else {
-                    fprintf(file_output, "non aggiunta\n");
-                    free(new_stn);
-                    free(cars);
-                }
+            }
+            new_stn = (stn *) calloc(1, sizeof(stn));
+            result = add_station(first_stn, new_stn, station, cars, len_cars, m_car);
+            if (result != NULL) {
+                first_stn = result;
+                fprintf(file_output, "aggiunta\n");
+                n_stations++;
+            }
+            else {
+                fprintf(file_output, "non aggiunta\n");
+                free(new_stn);
+                free(cars);
+            }
         }
         else if (strcmp(command, "demolisci-stazione") == 0) {
             station = (int)strtol(strtok(NULL, " "), NULL, 10);
@@ -188,10 +193,11 @@ int main (int argc, char *argv[])
     return 0;
 }
 
-stn* add_station (stn *first_stn, stn *new_stn, int station, int *cars, int len_cars) {
+stn* add_station (stn *first_stn, stn *new_stn, int station, int *cars, int len_cars, int m_car) {
     stn *curr_stn = first_stn;
 
     new_stn->id = station;
+    new_stn->m_car = m_car;
     new_stn->cars = cars;
     new_stn->len_cars = len_cars;
 
@@ -287,15 +293,18 @@ stn* del_station (stn *first_stn, int station, int *f) {
 int add_car (stn *first_stn, int station, int car) {
     stn *curr_stn = first_stn;
     int len_cars = 0;
-    while (curr_stn != NULL) {
-        if (curr_stn->id == station) {
-            len_cars = curr_stn->len_cars;
-            curr_stn->cars = (int *) realloc(curr_stn->cars, (curr_stn->len_cars + 1) * sizeof(int));
-            curr_stn->cars[len_cars] = car;
-            curr_stn->len_cars = len_cars + 1;
-            return 1;
-        }
+    while (curr_stn != NULL && curr_stn->id != station) {
         curr_stn = curr_stn->nxt;
+    }
+    if (curr_stn != NULL) {
+        if (curr_stn->m_car < car) {
+            curr_stn->m_car = car;
+        }
+        len_cars = curr_stn->len_cars;
+        curr_stn->cars = (int *) realloc(curr_stn->cars, (curr_stn->len_cars + 1) * sizeof(int));
+        curr_stn->cars[len_cars] = car;
+        curr_stn->len_cars = len_cars + 1;
+        return 1;
     }
     return 0;
 }
@@ -304,6 +313,7 @@ int del_car (stn *first_stn, int *new_cars, int station, int car) {
     stn *curr_stn = first_stn;
     int len_cars = 0;
     int k = 0;
+    int m_car = 0;
     while (curr_stn != NULL) {
         if (curr_stn->id == station) {
             for (int i = 0; i < curr_stn->len_cars; i++) {
@@ -314,12 +324,18 @@ int del_car (stn *first_stn, int *new_cars, int station, int car) {
                     for (int j = 0; j < curr_stn->len_cars; j++) {
                         if (curr_stn->cars[j] != -1) {
                             new_cars[k] = curr_stn->cars[j];
+                            if (new_cars[k] > m_car) {
+                                m_car = new_cars[k];
+                            }
                             k++;
                         }
                     }
                     free(curr_stn->cars);
                     curr_stn->cars = new_cars;
                     curr_stn->len_cars = len_cars;
+                    if (curr_stn->m_car == car) {
+                        curr_stn->m_car = m_car;
+                    }
                     return 1;
                 }
             }
@@ -328,6 +344,7 @@ int del_car (stn *first_stn, int *new_cars, int station, int car) {
         curr_stn = curr_stn->nxt;
     }
     return 0;
+    // si potrebbe ottimizzare iniziando gia a copiare l'array independentemente che la macchina sia presente o meno
 }
 
 int search_path (stn *first_stn, int stn_start, int stn_end, int *path, int n_stations) {
@@ -343,6 +360,8 @@ int search_path (stn *first_stn, int stn_start, int stn_end, int *path, int n_st
     stn *strt_stn = NULL;
     stn *nd_stn = NULL;
     stn *curr_stn = first_stn;
+    int m_car = 0;
+    int car = 0;
 
     if (direction == 1) {
         while (curr_stn != NULL) {
@@ -360,11 +379,18 @@ int search_path (stn *first_stn, int stn_start, int stn_end, int *path, int n_st
     else {
         while (curr_stn != NULL) {
             curr_stn->color = 0;
+            if (curr_stn->id >= stn_end) {
+                car = curr_stn->m_car;
+                if (car > m_car) {
+                    m_car = car;
+                }
+            }
             if (curr_stn->id == stn_end) {
                 nd_stn = curr_stn;
             }
             else if (curr_stn->id == stn_start) {
                 strt_stn = curr_stn;
+                // printf("max car: %d, percorso: %d -> %d\n", m_car, stn_end, stn_start);
                 break;
             }
             curr_stn = curr_stn->nxt;
@@ -378,12 +404,12 @@ int search_path (stn *first_stn, int stn_start, int stn_end, int *path, int n_st
     if (direction == 1) {
         queue->id = strt_stn;
         queue->father = NULL;
-        len_queue = bfs(nd_stn, queue, direction);
+        len_queue = bfs_dx(nd_stn, queue, direction);
     }
     else {
         queue->id = nd_stn;
         queue->father = NULL;
-        len_queue = bfs(strt_stn, queue, direction);
+        len_queue = bfs_sx(strt_stn, queue, direction, m_car);
     }
 
     if (len_queue != -1) {
@@ -423,12 +449,13 @@ int search_path (stn *first_stn, int stn_start, int stn_end, int *path, int n_st
     }
 }
 
-int bfs (stn *end_stn, q_str *queue, int direction) {
+int bfs_dx (stn *end_stn, q_str *queue, int direction) {
     stn *index = NULL;
     stn *curr_stn = NULL;
     q_str *v;
     int len_queue = 1;
     int i = 0;
+    int car = 0;
     while (i < len_queue) {
         v = queue + i;
         index = v->id;
@@ -444,35 +471,16 @@ int bfs (stn *end_stn, q_str *queue, int direction) {
         if (index == end_stn) {
             return i + 1;
         }
-        if (direction == 1) {
-            curr_stn = index;
-            if (curr_stn->nxt != NULL) {
-                curr_stn = index->nxt;
-                if (index->len_cars > 0) {
-                    while (curr_stn != NULL && curr_stn->id <= index->id + max_car(index->cars, index->len_cars) && curr_stn->id > index->id) {
-                        if (curr_stn->color == 0) {
-                            curr_stn->color = 1;
-                            (queue + len_queue)->id = curr_stn;
-                            (queue + len_queue)->father = index;
-                            len_queue++;
-                        }
-                        curr_stn = curr_stn->nxt;
-                    }
-                }
-            }
-        }
-        else {
-            curr_stn = index;
-            while (curr_stn != NULL && curr_stn->id <= end_stn->id) {
-                if (curr_stn->len_cars > 0) {
-                    if (index->id >= curr_stn->id - max_car(curr_stn->cars, curr_stn->len_cars) && index->id < curr_stn->id) {
-                        if (curr_stn->color == 0) {
-                            curr_stn->color = 1;
-                            (queue + len_queue)->id = curr_stn;
-                            (queue + len_queue)->father = index;
-                            len_queue++;
-                        }
-                    }
+        curr_stn = index;
+        if (curr_stn->nxt != NULL) {
+            curr_stn = index->nxt;
+            car = index->m_car;
+            while (curr_stn != NULL && curr_stn->id <= index->id + car && curr_stn->id > index->id) {
+                if (curr_stn->color == 0) {
+                    curr_stn->color = 1;
+                    (queue + len_queue)->id = curr_stn;
+                    (queue + len_queue)->father = index;
+                    len_queue++;
                 }
                 curr_stn = curr_stn->nxt;
             }
@@ -482,13 +490,47 @@ int bfs (stn *end_stn, q_str *queue, int direction) {
     return -1;
 }
 
-int max_car (int *cars, int len_cars) {
-    int car_m = 0;
-    for (int i = 0; i < len_cars; i++) {
-        if (cars[i] > car_m) 
-            car_m = cars[i];
+int bfs_sx (stn *end_stn, q_str *queue, int direction, int m_car) {
+    stn *index = NULL;
+    stn *curr_stn = NULL;
+    q_str *v;
+    int len_queue = 1;
+    int i = 0;
+    int car = 0;
+    while (i < len_queue) {
+        v = queue + i;
+        index = v->id;
+        #if debug_queue
+            if ((queue + i)->father != NULL) {
+                printf("[%d, %d]", (queue + i)->id->id, (queue + i)->father->id);
+            }
+            else {
+                printf("[%d, %d]", (queue + i)->id->id, -1);
+            }
+            printf("\n");
+        #endif
+        if (index == end_stn) {
+            return i + 1;
+        }
+        curr_stn = index;
+        if (curr_stn->nxt != NULL) {
+            curr_stn = index->nxt;
+            while (curr_stn != NULL && curr_stn->id <= index->id + m_car && curr_stn->id <= end_stn->id) {
+                car = curr_stn->m_car;
+                if (index->id >= curr_stn->id - car && index->id < curr_stn->id) {
+                    if (curr_stn->color == 0) {
+                        curr_stn->color = 1;
+                        (queue + len_queue)->id = curr_stn;
+                        (queue + len_queue)->father = index;
+                        len_queue++;
+                    }
+                }
+                curr_stn = curr_stn->nxt;
+            }
+        }
+        i++;
     }
-    return car_m;
+    return -1;
 }
 
 // -> implementare cars come una lista
--- run_test.sh
+++ run_test.sh
@@ -7,7 +7,7 @@ for i in {1..111}
 do
     echo test number: $i >>results_test.txt
     /usr/bin/time -h ./Sviluppo/list_ver Test/archivio_test_aperti/open_$i.txt >>results_test.txt 2>&1
-    diff -u Sviluppo/output.txt /Users/luca/Documents/Progetto-API/Test/archivio_test_aperti/open_$i.output.txt >>results_test.txt 2>&1
+    diff -u Sviluppo/output.txt Test/archivio_test_aperti/open_$i.output.txt >>results_test.txt 2>&1
     echo ' ' >>results_test.txt
     echo ' ' >>results_test.txt
 done
